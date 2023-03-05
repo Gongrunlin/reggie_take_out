8d5202f50816b44031a16d32648716971869d264
runlin <201879473@qq.com>
2023-03-05 09:47:36 +0800

    Reggie V1

---
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.itheima.reggie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * 静态资源映射
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
