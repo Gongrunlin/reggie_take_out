8acc2cea657b2f66e5115854668c3b98a8ae0dd7
Gong Runlin <201879473@qq.com>
2023-03-06 22:55:45 +0800

    增加登陆验证

--- src/main/java/com/itheima/reggie/controller/EmployController.java
+++ src/main/java/com/itheima/reggie/controller/EmployController.java
@@ -26,22 +26,52 @@ public class EmployController {
     @PostMapping("/login")
     public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
 
-        String password = DigestUtils.md5DigestAsHex(employee.getPassword().getBytes());
+        //1、将页面提交的密码password进行md5加密处理
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //2、根据页面提交的用户名username查询数据库
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(Employee::getUsername,employee.getUsername());
         Employee emp = employeeService.getOne(queryWrapper);
 
-        if (emp == null){
-            return R.error("Faile to login!");
+        //3、如果没有查询到则返回登录失败结果
+        if(emp == null){
+            return R.error("登录失败");
         }
-        
 
+        //4、密码比对，如果不一致则返回登录失败结果
+        if(!emp.getPassword().equals(password)){
+            return R.error("登录失败");
+        }
 
+        //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+        if(emp.getStatus() == 0){
+            return R.error("账号已禁用");
+        }
 
-        
+        //6、登录成功，将员工id存入Session并返回登录成功结果
+        request.getSession().setAttribute("employee",emp.getId());
+        return R.success(emp);
 
-
-        return null;
     }
+    /**
+     * 员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        //清理Session中保存的当前登录员工的id
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
+
+    /**
+     * 新增员工
+     *
+     */
+
+
 
 }
